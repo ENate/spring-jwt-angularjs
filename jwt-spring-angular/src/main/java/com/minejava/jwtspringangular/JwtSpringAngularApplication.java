88be6e5db5a96624522885eb10df492997adfb19
Nath <nathaniel@aims.ac.za>
2022-07-09 14:06:10 +0000

    Added a spring boot backend

---
+++ jwt-spring-angular/src/main/java/com/minejava/jwtspringangular/JwtSpringAngularApplication.java
@@ -0,0 +1,13 @@
+package com.minejava.jwtspringangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtSpringAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtSpringAngularApplication.class, args);
+	}
+
+}
---
+++ jwt-spring-angular/src/test/java/com/minejava/jwtspringangular/JwtSpringAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minejava.jwtspringangular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtSpringAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
