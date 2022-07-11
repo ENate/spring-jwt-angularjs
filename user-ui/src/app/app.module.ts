dfbced14fd51ff693d29953768064354338042b7
Nath <nathaniel@aims.ac.za>
2022-07-11 23:45:06 +0000

    created a new angular ui

--- user-ui/src/app/app.module.ts
+++ user-ui/src/app/app.module.ts
@@ -1,26 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
 import { AdminComponent } from './admin/admin.component';
+import { HomeComponent } from './home/home.component';
 import { UserComponent } from './user/user.component';
-import { LoginComponent } from './login/login.component';
 import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
 import { ForbiddenComponent } from './forbidden/forbidden.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
     AdminComponent,
+    HomeComponent,
     UserComponent,
-    LoginComponent,
     HeaderComponent,
+    LoginComponent,
     ForbiddenComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
